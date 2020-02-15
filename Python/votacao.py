099dcb70697b4f01e601a72194512872703dc7c1
Douglas Oliveira <56659549+Dellonath@users.noreply.github.com>
2020-02-15 15:09:41 -0300

    Update votacao.py

--- Python/votacao.py
+++ Python/votacao.py
@@ -1,26 +1,25 @@
 '''
 Uma empresa de pesquisas precisa tabular os resultados da seguinte enquete feita a um grande quantidade de organizações:
-
 "Qual o melhor Sistema Operacional para uso em servidores?"
-
 As possíveis respostas são:
-
 1- Windows Server
 2- Unix
 3- Linux
 4- Netware
 5- Mac OS
 6- Outro
-
 Você foi contratado para desenvolver um programa que leia o resultado da enquete e informe ao final o resultado da mesma. 
 O programa deverá ler os valores até ser informado o valor 0, que encerra a entrada dos dados. 
 Não deverão ser aceitos valores além dos válidos para o programa (0 a 6). Os valores referentes a cada uma das opções devem ser armazenados num vetor.
 Após os dados terem sido completamente informados, o programa deverá calcular a percentual de cada um dos concorrentes e informar o vencedor da enquete.
 O formato da saída foi dado pela empresa, e é o seguinte:
-
 '''
 # importanto os para clear
 import os 
+
+def votacao(nome, i):
+    global lista
+    print(f'O Sistema Operacional mais votado foi o {nome}, com {lista.count(i)} votos, correspondendo a {int(lista.count(i)*100/len(lista))}% dos votos.')
 
 # criando o painel
 
@@ -65,20 +64,19 @@ print(f'\nRESULTADO Total de votos {len(lista)}.\n ')
 i = 1
 vencedor = []
 while i <=6:
-	vencedor.append(lista.count(i))
-	i += 1
+    vencedor.append(lista.count(i))
+    i += 1
 i = vencedor.index(max(vencedor)) + 1
 # fornecendo o resultado
 if i == 1:
-    print(f'O Sistema Operacional mais votado foi o Windows, com {lista.count(1)} votos, correspondendo a {int(lista.count(1)*100/len(lista))}% dos votos.')
+    votacao('Windows', 1)
 elif i == 2:
-    print(f'O Sistema Operacional mais votado foi o Unix, com {lista.count(2)} votos, correspondendo a {int(lista.count(2)*100/len(lista))}% dos votos.')
+    votacao('Unix', 2)
 elif i == 3:
-    print(f'O Sistema Operacional mais votado foi o Linux, com {lista.count(3)} votos, correspondendo a {int(lista.count(3)*100/len(lista))}% dos votos.')
+    votacao('Linux', 3)
 elif i == 4:
-    print(f'O Sistema Operacional mais votado foi o Netware com {lista.count(4)} votos, correspondendo a {int(lista.count(4)*100/len(lista))}% dos votos.')
+    votacao('Netware', 4)
 elif i == 5:
-    print(f'O Sistema Operacional mais votado foi o MacOS, com {lista.count(5)} votos, correspondendo a {int(lista.count(5)*100/len(lista))}% dos votos.')
+    votacao('MacOS', 5)
 else:
-    print(f'O Sistema Operacional mais votado foi o Outros, com {lista.count(6)} votos, correspondendo a {int(lista.count(6)*100/len(lista))}% dos votos.')
-
+    votacao('Outros', 6)
