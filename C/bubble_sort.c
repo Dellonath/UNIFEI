aae0c27d84365d39a8da2f7a594ac1c1df6ef5ba
Douglas Oliveira <56659549+Dellonath@users.noreply.github.com>
2020-04-18 16:23:11 -0300

    Update bubble_sort.c

--- C/bubble_sort.c
+++ C/bubble_sort.c
@@ -2,45 +2,46 @@
 #include <stdlib.h>
 
 /*
-This algorithm orders an int vector with n elements drawn using Bubble Sort
+This algorithm orders a int vector with n elements drawn using Bubble Sort by recurrence.
 */
 
 void bubble_sort(int v[], int n, int i, int j){
     
     if(j != n){
-        if(v[i] > v[j]){
+        if(v[i] > v[j]){ // verify and swap the elements
             int aux;
             aux = v[i];
             v[i] = v[j];
             v[j] = aux;
-            bubble_sort(v, n, 0, 1);
+            bubble_sort(v, n, 0, 1); // returns to position 0
         }else{
-            bubble_sort(v, n, i + 1, j + 1);
-        }
+            bubble_sort(v, n, i + 1, j + 1); // forward
+        } 
     }else{
         return ;
     }
 }
 
-int main(){
+void main(){
     int *v, valor, n;
     printf("Write the size of vector: ");
     scanf("%d", &n);
     
-    v = (int *) malloc(n * sizeof(int));
+    v = (int *) malloc(n * sizeof(int)); // allocates a vector with n elements
     
     for(int i = 0;i < n; i++){
-        v[i] = rand() % 100;
+        v[i] = rand() % 100; // function rand() return a random int number
     }
     
     for(int i = 0;i < n; i++){
-        printf("%d ", v[i]);
+        printf("%d ", v[i]); // print the vector before the bubble sort
     }
     
     bubble_sort(v, n, 0, 1);
+    
     printf("\n");
     for(int i = 0;i < n; i++){
-        printf("%d ", v[i]);
+        printf("%d ", v[i]); // print the vector after the bubble sort
     }
-    return 0;
 }
+
