79b13d0a07adcfa4787c5b5dc8b566d2a1dff3bc
Douglas Oliveira <56659549+Dellonath@users.noreply.github.com>
2020-10-07 05:56:26 -0300

    Update recursive_karatsuba_algorithm.c

--- C/recursive_karatsuba_algorithm.c
+++ C/recursive_karatsuba_algorithm.c
@@ -1,3 +1,6 @@
+// douglas raimundo de oliveira silva 11
+// anderson leandro dos reis 11
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -52,16 +55,26 @@ void shift(int sh, int n, signed char *A){
 }
 
 void shift_r(int sh, int n, singledigit *A, int m, singledigit *B){
+  /*
+  esta função shifta o número para direita, é como se dividisse o número por 10^sh
+  por exemplo, 12345 shifitado 2 vezes fica 00123
+  */
   int aux = n - sh; 
   for(int i = n - aux - 1, k = m - 1 ; i >= 0; i--, k--) B[k] = A[i];
 }
 
-void rest(int sh, int n, int m, singledigit *A, singledigit *B){
+void rest(int n, int m, singledigit *A, singledigit *B){
+  /*
+  esta função pega toma os m números menos significativos de um vetor de tamanho n
+  e coloca em um vetor de tamanho m, por exemplo, 12345 para um vetor de 3 posições
+  ficaria 345
+  */
   for(int i = n - 1, k = m - 1; k >= 0; i--, k--) B[k] = A[i];
 }
 
 void copy(int n, singledigit *A, int m, singledigit *B){
-    for(int i = n - 1, k = m - 1; i >= 0; i--, k--) B[k] = A[i];
+  // essa função apenas copia um vetor no outro da direita para a esquerda
+  for(int i = n - 1, k = m - 1; i >= 0; i--, k--) B[k] = A[i];
 }
 
 void traditional_mult(int n, singledigit *A, singledigit *B, int m, singledigit *C){
@@ -102,6 +115,7 @@ void karatsuba(int n, singledigit *X, singledigit *Y, int max, singledigit *Z){
 
   int m = ceil((float) n/2);
 
+  // declarando todos os vetores com seus respectivos tamanhos
   singledigit *P = calloc(m, sizeof(singledigit));
   singledigit *Q = calloc(m, sizeof(singledigit));
   singledigit *R = calloc(m, sizeof(singledigit));
@@ -116,35 +130,37 @@ void karatsuba(int n, singledigit *X, singledigit *Y, int max, singledigit *Z){
   singledigit *Kaux = calloc(2*(m+1), sizeof(singledigit));
   singledigit *Zaux = calloc(max, sizeof(singledigit));
   
-  if(n%2 == 0){
+  if(n%2 == 0){ //tratando a quantidade de shifts quando n for par e quando for ímpar
     shift_r(m, n, X, m, P);     
     shift_r(m, n, Y, m, R);
   }else{
-    shift_r(m-1, n, X, m, P);
-    shift_r(m-1, n, Y, m, R);
+    shift_r(m-1, n, X, m, P); // necessário shiftar uma vez a menos quando ímpar, pois
+    shift_r(m-1, n, Y, m, R); // quando n é ímpar não existe é falhado ao dividir ao meio
   }
 
-  rest(m, n, m, X, Q);
-  rest(m, n, m, Y, S); 
+  rest(n, m, X, Q); // tomando os restos (pegando os números menos significativos)
+  rest(n, m, Y, S); 
 
-  sum(m, P, m, Q, m+1, P_Q);
-  sum(m, R, m, S, m+1, R_S);
+  sum(m, P, m, Q, m+1, P_Q); // somando P + Q
+  sum(m, R, m, S, m+1, R_S); // somando R + S
 
-  karatsuba(m, P, R, 2*m, PR);
-  karatsuba(m, Q, S, 2*m, QS);
-  karatsuba(m+1, P_Q, R_S, 2*(m+1), K);
+  karatsuba(m, P, R, 2*m, PR); // multiplicando P e R
+  karatsuba(m, Q, S, 2*m, QS); // multiplicando Q e S
+  karatsuba(m+1, P_Q, R_S, 2*(m+1), K); //multiplicando (P + Q)*(R + S)
 
-  subtract(2*(m+1), K, 2*m, PR, 2*(m+1), Kaux);
-  subtract(2*(m+1), Kaux, 2*m, QS, 2*(m+1), K);
+  subtract(2*(m+1), K, 2*m, PR, 2*(m+1), Kaux); // (P + Q)(R + S) - PR
+  subtract(2*(m+1), Kaux, 2*m, QS, 2*(m+1), K); // (P + Q)(R + S) - PR - QS
 
-  copy(2*m, PR, max, PRS);
-  shift(2*m, max, PRS);
+  copy(2*m, PR, max, PRS); // copiando PR em PRS, PRS é um vetor de tamanho PR + m
+  shift(2*m, max, PRS); // é necessário armazenar o tamanho PR em um vetor maior para shiftar m vezes
 
-  copy(2*(m+1), K, 2*(m+1)+m, KS);
-  shift(m, 2*(m+1)+m, KS);
+  copy(2*(m+1), K, 2*(m+1)+m, KS); // mesma coisa como feito com PR
+  shift(m, 2*(m+1)+m, KS); // [(P + Q)(R + S) - PR - QS]^m
 
-  sum(max, PRS, 2*(m+1)+m, KS, max, Zaux);
-  sum(max, Zaux, 2*m, QS, max, Z);
+  sum(max, PRS, 2*(m+1)+m, KS, max, Zaux); // PR^2m + [(P + Q)(R + S) - PR - QS]^m
+  sum(max, Zaux, 2*m, QS, max, Z); // PR^2m + [(P + Q)(R + S) - PR - QS]^m + QS
+
+  // libera geral (fim da festa)
   free(P);free(Q);free(R);free(S);free(P_Q);
   free(R_S);free(PR);free(PRS);free(QS);free(K);
   free(KS);free(Kaux);free(Zaux);
